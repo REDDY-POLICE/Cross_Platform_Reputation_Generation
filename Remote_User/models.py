adc97bacd051b0dc90af36bcaca0f8300df639b2
Reddy-Police <reddy.police125@gmail.com>
2023-09-22 19:13:20 +0530

    all added

---
+++ Remote_User/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+from django.db.models import CASCADE
+
+
+class ClientRegister_Model(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    password = models.CharField(max_length=10)
+    phoneno = models.CharField(max_length=10)
+    country = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    gender= models.CharField(max_length=30)
+    address= models.CharField(max_length=30)
+
+
+class predict_reputation_generation(models.Model):
+
+    pid= models.CharField(max_length=300)
+    ProductId= models.CharField(max_length=300)
+    Post_Review= models.CharField(max_length=30000)
+    Platform= models.CharField(max_length=300)
+    Rating= models.CharField(max_length=300)
+    Date= models.CharField(max_length=300)
+    Helpful= models.CharField(max_length=300)
+    Prediction= models.CharField(max_length=300)
+
+class detection_accuracy(models.Model):
+
+    names = models.CharField(max_length=300)
+    ratio = models.CharField(max_length=300)
+
+class detection_ratio(models.Model):
+
+    names = models.CharField(max_length=300)
+    ratio = models.CharField(max_length=300)
+
+
+
